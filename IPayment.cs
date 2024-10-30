f89b1e385809a9f0dbb07618133f754eec6cd819
Suneliya <149096669+sunnadi@users.noreply.github.com>
2024-10-30 10:42:54 +0500

    Add files via upload

---
+++ IPayment.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace PaymentSystemAdapterExample
+{
+    public interface IPaymentProcessor
+    {
+        void ProcessPayment(double amount);
+    }
+    public class PayPalPaymentProcessor : IPaymentProcessor
+    {
+        public void ProcessPayment(double amount)
+        {
+            Console.WriteLine($"Обработка платежа на сумму {amount} руб. через PayPal.");
+        }
+    }
+
+    public class StripePaymentService
+    {
+        public void MakeTransaction(double totalAmount)
+        {
+            Console.WriteLine($"Проведение транзакции на сумму {totalAmount} руб. через Stripe.");
+        }
+    }
+
+    public class StripePaymentAdapter : IPaymentProcessor
+    {
+        private readonly StripePaymentService _stripeService;
+
+        public StripePaymentAdapter(StripePaymentService stripeService)
+        {
+            _stripeService = stripeService;
+        }
+
+        public void ProcessPayment(double amount)
+        {
+            _stripeService.MakeTransaction(amount);
+        }
+    }
+    public class SquarePaymentService
+    {
+        public void ProcessPaymentAmount(double paymentAmount)
+        {
+            Console.WriteLine($"Проведение платежа на сумму {paymentAmount} руб. через Square.");
+        }
+    }
+
+    public class SquarePaymentAdapter : IPaymentProcessor
+    {
+        private readonly SquarePaymentService _squareService;
+
+        public SquarePaymentAdapter(SquarePaymentService squareService)
+        {
+            _squareService = squareService;
+        }
+
+        public void ProcessPayment(double amount)
+        {
+            _squareService.ProcessPaymentAmount(amount);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPaymentProcessor paypalProcessor = new PayPalPaymentProcessor();
+            paypalProcessor.ProcessPayment(100.0);
+
+            StripePaymentService stripeService = new StripePaymentService();
+            IPaymentProcessor stripeAdapter = new StripePaymentAdapter(stripeService);
+            stripeAdapter.ProcessPayment(200.0);
+
+            SquarePaymentService squareService = new SquarePaymentService();
+            IPaymentProcessor squareAdapter = new SquarePaymentAdapter(squareService);
+            squareAdapter.ProcessPayment(300.0);
+        }
+    }
+}
+
---
+++ coffeelab.cs
@@ -0,0 +1,190 @@
+﻿public interface IBeverage
+{
+    double GetCost();
+    string GetDescription();
+
+}
+
+public class Coffee : IBeverage
+{
+    public double GetCost()
+
+    {
+        return 50.0; 
+
+    }
+    public string GetDescription()
+    {
+        return "Coffee";
+    }
+
+}
+
+public abstract class BeverageDecorator : IBeverage
+{
+    protected IBeverage _beverage;
+
+    public BeverageDecorator(IBeverage beverage)
+    {
+        _beverage = beverage;
+    }
+
+    public virtual double GetCost()
+    {
+        return _beverage.GetCost(); 
+
+    }
+
+    public virtual string GetDescription()
+    {
+        return _beverage.GetDescription();
+
+    }
+
+}
+
+public class MilkDecorator : BeverageDecorator
+
+{
+    public MilkDecorator(IBeverage beverage) : base(beverage) { }
+
+
+    public override double GetCost()
+    {
+        return base.GetCost() + 10.0; 
+
+    }
+
+    public override string GetDescription()
+
+    {
+        return base.GetDescription() + ", Milk";
+
+    }
+
+}
+
+public class SugarDecorator : BeverageDecorator
+{
+    public SugarDecorator(IBeverage beverage) : base(beverage) { }
+
+
+    public override double GetCost()
+
+    {
+        return base.GetCost() + 5.0; 
+
+    }
+
+
+    public override string GetDescription()
+    {
+        return base.GetDescription() + ", Sugar";
+    }
+
+}
+
+public class ChocolateDecorator : BeverageDecorator
+
+{
+
+    public ChocolateDecorator(IBeverage beverage) : base(beverage) { }
+
+
+    public override double GetCost()
+
+    {
+        return base.GetCost() + 15.0; 
+    }
+
+
+    public override string GetDescription()
+
+    {
+        return base.GetDescription() + ", Chocolate";
+
+    }
+
+}
+
+public class CinnamonDecorator : BeverageDecorator
+
+{
+
+    public CinnamonDecorator(IBeverage beverage) : base(beverage) { }
+
+
+    public override double GetCost()
+
+    {
+        return base.GetCost() + 15.0; 
+    }
+
+
+    public override string GetDescription()
+
+    {
+        return base.GetDescription() + ", Cinnamon";
+
+    }
+
+}
+
+public class VanillaDecorator : BeverageDecorator
+
+{
+
+    public VanillaDecorator(IBeverage beverage) : base(beverage) { }
+
+
+    public override double GetCost()
+
+    {
+        return base.GetCost() + 15.0;
+    }
+
+
+    public override string GetDescription()
+
+    {
+        return base.GetDescription() + ", Vanilla";
+
+    }
+
+}
+
+
+class Program
+
+{
+
+    static void Main(string[] args)
+
+    {
+        IBeverage beverage = new Coffee();
+        Console.WriteLine($"{beverage.GetDescription()} : {beverage.GetCost()}");
+
+
+        beverage = new MilkDecorator(beverage);
+        Console.WriteLine($"{beverage.GetDescription()} : {beverage.GetCost()}");
+
+
+        beverage = new SugarDecorator(beverage);
+        Console.WriteLine($"{beverage.GetDescription()} : {beverage.GetCost()}");
+
+
+        beverage = new ChocolateDecorator(beverage);
+        Console.WriteLine($"{beverage.GetDescription()} : {beverage.GetCost()}");
+
+        beverage = new CinnamonDecorator(beverage);
+        Console.WriteLine($"{beverage.GetDescription()} : {beverage.GetCost()}");
+
+
+        beverage = new VanillaDecorator(beverage);
+        Console.WriteLine($"{beverage.GetDescription()} : {beverage.GetCost()}");
+
+
+
+    }
+
+}
